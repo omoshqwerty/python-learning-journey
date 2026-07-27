58a3798eacbbf024cadf8dc6f71f74e8917e5312
omoshqwerty <domollo67@gmail.com>
2026-07-28 00:39:27 +0300

    Add word analysis functionality to word_analyzer.py

--- topic-05-loops/word_analyzer.py
+++ topic-05-loops/word_analyzer.py
@@ -1,1 +1,11 @@
-word_an
+fav = input("What is your favourite language and why? ")
+words = fav.split(" ")
+total = len(words)
+long = max(words, key=len)
+capitalized_words = []
+for w in words:
+    capitalized_words.append(w.capitalize())
+title_sentence = " ".join(capitalized_words)
+print(f"Sentence: {title_sentence}")
+print(f"Total words: {total}")
+print(f"Longest word: {long}")
